5f963fdb47c788fc024d2380f62441f49a258219
Sharmistha Mohapatra <sharmisthaimohapatra@gmail.com>
2026-03-01 16:42:06 +0530

    fourth commit

--- UC4.java
+++ UC4.java
@@ -1,13 +1,13 @@
-public class Oopsuc4 {
+public class UC4{
     public static void main(String[] args) {
         String[] bannerLines = new String[7];
-        bannerLines[0] = String.join(" ", " ** ", " ** ", " ** ", " **** ");
+        bannerLines[0] = String.join(" ", " **** ", " **** ", " ****** ", " ******** ");
         bannerLines[1] = String.join(" ", " *  * ", " *  * ", " *    * ", " *        ");
         bannerLines[2] = String.join(" ", " *  * ", " *  * ", " *    * ", " *        ");
-        bannerLines[3] = String.join(" ", " *  * ", " *  * ", " ** ", " **** ");
+        bannerLines[3] = String.join(" ", " *  * ", " *  * ", " ****** ", " ******** ");
         bannerLines[4] = String.join(" ", " *  * ", " *  * ", " *      ", "        * ");
         bannerLines[5] = String.join(" ", " *  * ", " *  * ", " *      ", "        * ");
-        bannerLines[6] = String.join(" ", " ** ", " ** ", " *      ", " **** ");
+        bannerLines[6] = String.join(" ", " **** ", " **** ", " *      ", " ******** ");
         for (String line : bannerLines) {
             System.out.println(line);
         }
