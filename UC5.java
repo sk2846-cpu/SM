c1e71ea4f8e45590c530a794d5ed98a4eede6d46
Sharmistha Mohapatra <sharmisthaimohapatra@gmail.com>
2026-03-01 16:44:32 +0530

    fifth commit

--- UC5.java
+++ UC5.java
@@ -1,13 +1,13 @@
-public class Oopsuc5 {
+public class UC5{
     public static void main(String[] args) {
         String[] bannerLines = {
-            String.join(" ", " ** ", " ** ", " ** ", " **** "),
+            String.join(" ", " **** ", " **** ", " ****** ", " ******** "),
             String.join(" ", " *  * ", " *  * ", " *    * ", " *        "),
             String.join(" ", " *  * ", " *  * ", " *    * ", " *        "),
-            String.join(" ", " *  * ", " *  * ", " ** ", " **** "),
+            String.join(" ", " *  * ", " *  * ", " ****** ", " ******** "),
             String.join(" ", " *  * ", " *  * ", " *      ", "        * "),
             String.join(" ", " *  * ", " *  * ", " *      ", "        * "),
-            String.join(" ", " ** ", " ** ", " *      ", " **** ")
+            String.join(" ", " **** ", " **** ", " *      ", " ******** ")
         };
         for (String line : bannerLines) {
             System.out.println(line);
